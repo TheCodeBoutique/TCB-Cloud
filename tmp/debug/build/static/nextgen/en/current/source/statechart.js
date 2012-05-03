f8975f3fa4ccc6d6fc2df9d9aadc0b0afccdc613
Chad Eubanks <ceubanks@thecodeboutique.com>
2012-05-02 23:15:09 -0700

    iniital commit

--- tmp/debug/build/static/nextgen/en/current/source/statechart.js
+++
@@ -1,8 +0,0 @@
-Nextgen.statechart = SC.Statechart.create({
-
-  initialState: 'readyState',
-  
-  readyState: SC.State.plugin('Nextgen.ReadyState'),
-  // someOtherState: SC.State.plugin('Nextgen.SomeOtherState')
-
-});; if ((typeof SC !== 'undefined') && SC && SC.Module && SC.Module.scriptDidLoad) SC.Module.scriptDidLoad('nextgen');
