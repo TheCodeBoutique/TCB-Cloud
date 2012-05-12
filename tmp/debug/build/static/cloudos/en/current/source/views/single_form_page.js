54ad8d49c249e40df1c3e4341f3e1da20a8f8737
Chad Eubanks <ceubanks@thecodeboutique.com>
2012-05-11 20:47:57 -0700

    fixed binding issues... rewrite WIP

--- tmp/debug/build/static/cloudos/en/current/source/views/single_form_page.js
+++
@@ -1,9 +0,0 @@
-COS.singleFormPage = SC.Page.design ({
-	
-	formView: COS.singleFormView.create ({
-		layout: { top: 0, bottom: 0, left: 0, right: 0 },
-		singleFormTitleBinding: 'Nextgen.deployMessageViewController.singleFormTitle'
-	}),
-		
-	
-}); if ((typeof SC !== 'undefined') && SC && SC.Module && SC.Module.scriptDidLoad) SC.Module.scriptDidLoad('cloudos');
