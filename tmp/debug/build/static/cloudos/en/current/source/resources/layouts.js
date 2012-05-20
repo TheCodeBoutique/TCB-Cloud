264b67deaf125789dd4c00deb339c8ff46383736
Chad Eubanks <ceubanks@thecodeboutique.com>
2012-05-19 19:00:40 -0700

    pushing latest

--- tmp/debug/build/static/cloudos/en/current/source/resources/layouts.js
+++
@@ -1,6 +0,0 @@
-SC.mixin(COS,{
-PANE_LAYOUT: {
-	 DEPLOY_MENU_LAYOUT: {centerX: 0, centerY: 0, height: 248, width: 448 }
-}
-})
-; if ((typeof SC !== 'undefined') && SC && SC.Module && SC.Module.scriptDidLoad) SC.Module.scriptDidLoad('cloudos');
--- tmp/debug/build/static/nextgen/en/current/source/controllers/applications_controller.js
+++
@@ -1,4 +0,0 @@
-Nextgen.applicationsController = SC.ArrayController.create({
-  content: ''
-});
-; if ((typeof SC !== 'undefined') && SC && SC.Module && SC.Module.scriptDidLoad) SC.Module.scriptDidLoad('nextgen');
